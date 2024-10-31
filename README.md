a1b766a81e8b77904dbedcea7a5e41f330b9ecd4
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-10-31 11:21:26 +0100

    plotting in readme

--- README.md
+++ README.md
@@ -9,6 +9,40 @@
 
 Julia implementation of the $\pi\pi$ scattering amplitudes in S,P, and F waves following the GKPY11 model, from the ["Pion-pion scattering amplitude, IV"](https://inspirehep.net/literature/889131).
 
+
+## Installation
+
+```julia
+pkg> add https://github.com/mmikhasenko/GKPY11.jl.git
+```
+
+## Usage
+
+Function `δ0`, `δ1`, and `δ2` give values of the phase shifts in S, P, and F waves, respectively, as functions of the squared mass of the $\pi\pi$ system.
+
+Signature of each function is
+```
+δ0(s; pars)
+```
+where the parameters are set to defualt values given by `s_wave_pars`, `p_wave_pars`, and `f_wave_pars`.
+
+```julia
+using GKPY11
+using Plots
+
+let
+    support = (0.1, 2.0)
+    plot(xlab = "m(ππ) [GeV]",
+        ylab = "phase shift [rad]",
+        title = "scattering phase shifts", leg=:topleft)
+    plot!(e -> δ0(e^2), support..., label = "δ0", lw = 2)
+    plot!(e -> δ1(e^2), support..., label = "δ1", lw = 2)
+    plot!(e -> δ3(e^2), support..., label = "δ3", lw = 2)
+    vspan!([GKPY11.s_wave_pars.m_boundary3, support[2]], alpha=0.1)
+end
+```
+
+
 ## How to Cite
 
 Please cite the original paper, [link to inspire](https://inspirehep.net/literature/889131).
--- src/s-wave.jl
+++ src/s-wave.jl
@@ -3,7 +3,7 @@
     m_boundary1::Float64 = 0.85
     m_boundary3::Float64 = 1.42
     # interval 1
-    b_coeffs::NTuple{4,Float64} = (7.14, -25.3, -33.2, -26.2)
+    b_coeffs::NTuple{4, Float64} = (7.14, -25.3, -33.2, -26.2)
     z0::Float64 = 0.13957 # Adler zero at z0^2/2
     # interval 1
     d0::Float64 = 226.5
@@ -45,7 +45,7 @@ end
 
 function cotδ0_interval1(s; pars = s_wave_pars)
     @unpack b_coeffs, z0 = pars
-    # 
+    #
     w = conformal_w(s; s0 = (2 * mK)^2)
     conf_expansion = sum(enumerate(b_coeffs)) do (i, b)
         b * w^(i - 1)
@@ -59,44 +59,49 @@ end
 function δ0_interval1(s; pars = s_wave_pars)
     _cotδ0_interval1 = cotδ0_interval1(s; pars)
     _δ0_interval1 = atan(1 / _cotδ0_interval1)
-    return 180.0 / π * (_δ0_interval1 < 0 ? _δ0_interval1 + π : _δ0_interval1)
+    return _δ0_interval1 < 0 ? _δ0_interval1 + π : _δ0_interval1
 end
 
 function δ0_interval2(s; pars = s_wave_pars)
     @unpack m_boundary1, d0, c = pars
-    # 
+    #
     ϵ = 1e-6
     δ1_boundary1 = δ0_interval1(m_boundary1^2; pars)
     dδ1_dm_boundary1 =
         (δ0_interval1(m_boundary1^2 + ϵ; pars) - δ0_interval1(m_boundary1^2 - ϵ; pars)) /
         (2ϵ)
-    # 
+    #
+    δ1_boundary1_deg = δ1_boundary1 / π * 180
+    dδ1_dm_boundary1_deg = dδ1_dm_boundary1 / π * 180
+    #
     a_k2 = s > (2 * mK)^2 ? kK(s) : sqrt(mK^2 - s / 4)
     k2_m = sqrt(mK^2 - m_boundary1^2 / 4)
-    return d0 * (1 - a_k2 / k2_m)^2 +
-           δ1_boundary1 * a_k2 / k2_m * (2 - a_k2 / k2_m) +
-           a_k2 * (k2_m - a_k2) * (8 * dδ1_dm_boundary1 + c * (k2_m - a_k2) / (mK^3))
+    _δ0 = d0 * (1 - a_k2 / k2_m)^2 +
+          δ1_boundary1_deg * a_k2 / k2_m * (2 - a_k2 / k2_m) +
+          a_k2 * (k2_m - a_k2) * (8 * dδ1_dm_boundary1_deg + c * (k2_m - a_k2) / (mK^3))
+    return _δ0 / 180.0 * π
 end
 
 function δ0_interval3(s; pars = s_wave_pars)
     @unpack d0, b, d, c_coeff = pars
     k2 = kK(s)
-    return d0 +
-           b * (k2 / mK)^2 +
-           c_coeff * (k2 / mK)^4 +
-           d * (s > (2 * mη)^2 ? (kη(s) / mη)^2 : 0)
+    _δ0 = d0 +
+          b * (k2 / mK)^2 +
+          c_coeff * (k2 / mK)^4 +
+          d * (s > (2 * mη)^2 ? (kη(s) / mη)^2 : 0)
+    return _δ0 / 180.0 * π
 end
 
 function δ0_inverval4(s; pars = s_wave_pars)
     @unpack m_boundary3 = s_wave_pars
-    # 
+    #
     ϵ = 1e-6
     δ3_boundary3 = δ0_interval3(m_boundary3^2; pars)
     dδ3_dm_boundary3 =
         (δ0_interval3(m_boundary3^2 + ϵ; pars) - δ0_interval3(m_boundary3^2 - ϵ; pars)) /
         (2e-6)
-    # 
-    a = 360.0
+    #
+    a = 2π
     alpha = dδ3_dm_boundary3 / (a - δ3_boundary3)
     p = dδ3_dm_boundary3 / (a * alpha) * exp(alpha * (m_boundary3)^2)
     return a * (1 - p * exp(-alpha * s))
